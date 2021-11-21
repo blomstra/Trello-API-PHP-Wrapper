4dec44c53dcd074fdf68128ae9ef5e3251f24a6b
Bertrand Kintanar <bertrand.kintanar@gmail.com>
2021-11-21 22:17:21 +0800

    fix: Make Label color optional

--- src/Models/Label.php
+++ src/Models/Label.php
@@ -41,9 +41,9 @@ class Label extends BaseObject
             throw new InvalidArgumentException('Missing required field "name"');
         }
 
-        if (empty($this->color)) {
-            throw new InvalidArgumentException('Missing required filed "color" - color that the label should have');
-        }
+        // if (empty($this->color)) {
+        //     throw new InvalidArgumentException('Missing required filed "color" - color that the label should have');
+        // }
 
         if (empty($this->idBoard)) {
             throw new InvalidArgumentException('Missing required filed "idBoard" - id of the board that the label should be added to');
